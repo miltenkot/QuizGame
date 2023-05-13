0480520f0461a2b5309e51097eba70d14513a39c
miltenkot <miltenkot@gmail.com>
2023-05-13 11:27:42 +0200

    Initial commit.

---
+++ QuizGame/QuizGame/QuizGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizGameApp.swift
+//  QuizGame
+//
+//  Created by Bartłomiej on 13/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct QuizGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
